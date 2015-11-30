5033bf39dbe53ce4f21119b2679e1b4912160fc4
MikeKutter <mkutter@yahoo.com>
2015-11-30 14:52:54 -0500

    Made changes to merge survey DB stuff from outside project

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var passport = require('./lib/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var survey = require('./routes/survey');
 
 var app = express();
 
@@ -51,6 +52,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/survey', survey);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
