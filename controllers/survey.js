e26e22c7645649d9738e70953d8261b8833bc800
MikeKutter <mkutter@yahoo.com>
2015-12-01 09:16:57 -0500

    Added initial code for getting a survey

--- controllers/survey.js
+++ controllers/survey.js
@@ -5,6 +5,7 @@
 
 var Survey = require('../models').model('Survey');
 
+
 module.exports = {
     deny : function(req, res) {
             res.sendStatus(405);
@@ -41,6 +42,29 @@ module.exports = {
             }).catch(function(err) {
                 next(err);
             });
+        }
+    },
+
+    survey : {
+    get : function(req, res) {
+
+
+        // Survey.find({_id:"565cb466f29f770c1a23fb28"}, { __v: 0 }).exec().then(
+        // Survey.find({_id:"565cb466f29f770c1a23fb28"}).exec().then(
+        // Survey.find().exec().then(
+            Survey.findOne().exec().then(
+        // Survey.find({surveyURL:"ZXY"}).exec().then(
+             function(survey) {
+                res.json(survey);
+          }).catch(function(error) {
+            next(error);
+          });
+
+
+        // res.json({
+        //         title : (req.user && req.user.userName) || 'Nobody'
+        //     });
+
         }
     }
 };
